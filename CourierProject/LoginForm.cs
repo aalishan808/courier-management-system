3532cdd49c4572c29e2b599a79db05a756e53703
aalishan808 <92679900+aalishan808@users.noreply.github.com>
2023-06-19 09:02:23 +0500

    Update LoginForm.cs

--- CourierProject/LoginForm.cs
+++ CourierProject/LoginForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.CodeDom;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using Oracle.ManagedDataAccess.Client;
 
 namespace CourierProject
@@ -15,34 +16,69 @@ namespace CourierProject
     public partial class LoginForm : Form
     {
         OracleConnection con;
+        public static String cnic;
         public LoginForm()
         {
             InitializeComponent();
+            
+           
+
         }
        
         private void signup_button1_Click(object sender, EventArgs e)
         {
            this.Close();
         }
-
+       
         private void login_button1_Click(object sender, EventArgs e)
         {
+           
             String email = textBox1.Text;
             String password = textBox2.Text;
-            String admincnic = cnictextbox.Text;
+            cnic = cnictextbox.Text;
+            String pas = "";
+            String email_ = "";
+            String admincnic = "";
             try
             {
                 con.Open();
                 OracleCommand cmd = con.CreateCommand();
-                cmd.CommandText = $"SELECT * FROM ADMIN_ WHERE email='{email}' AND password_ = '{password}'AND adminCNIC = '{admincnic}'";
+                cmd.CommandText = $"SELECT * FROM ADMIN_ WHERE aemail='{email}' AND apassword_ = '{password}'AND adminCNIC = '{cnic}'";
                 cmd.CommandType = CommandType.Text;
                 cmd.ExecuteNonQuery();
-                con.Close();
-                UserForm f = new UserForm();
-                f.admincnic_ = admincnic;
-                MainForm form = new MainForm();
-                form.Show();
+                OracleDataReader reader = cmd.ExecuteReader();
                 
+                if (reader.HasRows)
+
+                {
+
+                    reader.Read();
+                    pas = reader["apassword_"].ToString();
+                    email_ = reader["aemail"].ToString();
+                    admincnic = reader["adminCNIC"].ToString() ;
+                    cnic = admincnic;
+                    UserForm f1 = new UserForm() ;
+                    
+                    if (email == email_ &&  pas == password && cnic == admincnic){
+                        
+                        MainForm form1 = new MainForm();
+                        form1.Show();
+                    }
+                    else{
+                        cor.Text = "wrong data";
+                    
+                    }
+                    con.Close();
+                    //  DataTable dataTable = new DataTable();
+                    //dataTable.Load(reader);
+
+
+                    
+                }
+
+
+                
+
             }
             catch (Exception ex)
             {
